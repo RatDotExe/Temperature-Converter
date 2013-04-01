6bcddbe7a3db0a74844919c48c357eaf45da8900
Zachary <rat@ratdotexe.com>
2013-04-01 16:38:22 -0400

    Fixed Fahrenheit, added multiple conversion option.

--- Temperature Converter.cpp
+++ Temperature Converter.cpp
@@ -10,11 +10,17 @@
 using namespace std;
 int main() {
 	
+	int five = 5;
+	int nine = 9;
 	int a = 32;
 	double b = 1.8;
 	int t;
 	int r;
 	string u;
+	string another = "y";
+	
+	while (another == "y") {
+		
 	
 	cout << "What's the unit?\n";
 	cin >> u;
@@ -28,7 +34,7 @@ int main() {
 	}
 		
     	if (u == "fahrenheit") {
-			r = t - a / b;
+			r = (t-a) * five / nine;
 			u = "C";
 	}
 	
@@ -38,10 +44,13 @@ int main() {
 	}
 	
 	if (u == "f") {
-		r = t - a / b;
+		r = (t-a) * five / nine;
 	    u = "C";
 	}
-	cout << "Result: " << r << u;
+	cout << "Result: " << r << u << "\n";
 	
+	cout << "Type y to do another conversion. Type n to quit.\n";
+	cin >> another;
+	}
 	return 0;
 }
