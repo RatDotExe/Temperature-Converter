4c6ebad39179079e55fc7f08b6f452b4be8773ce
Zachary <rat@ratdotexe.com>
2013-04-01 13:25:49 -0400

    First Commit

---
+++ Temperature Converter.cpp
@@ -0,0 +1,44 @@
+//
+// Rat_Dot_Exe's Temperature Converter
+// Written in C++
+//
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+int main() {
+	
+	int a = 32;
+	double b = 1.8;
+	int t;
+	int r;
+	string u;
+	
+	cout << "What's the unit?\n";
+	cin >> u;
+	
+	cout << "What's the number?\n";
+	cin >> t;
+	
+	if (u == "celcius") {
+			r = t * b + a;
+		    u = "F";
+		}
+		
+    if (u == "fahrenheit") {
+			r = t - a / b;
+			u = "C";
+		}
+	if (u == "c") {
+		r = t * b + a;
+		u = "F";
+	}
+	
+	if (u == "f") {
+		r = t - a / b;
+	    u = "C";
+	}
+	cout << "Result: " << r << u;
+}
